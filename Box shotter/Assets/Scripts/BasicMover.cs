c0a6db9575d835b4b31f0bf3c42a026e06018097
Nirjorsaharudro <rudrosaha103863@gmail.com>
2024-09-22 12:19:50 +0600

    final_product
    Former-commit-id: d20b6534d651e08be5f1cbf781f3d3f0c58a7ee9

---
+++ Box shotter/Assets/Scripts/BasicMover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMover : MonoBehaviour
+{
+    public enum motionDirection {Spin,Horizontal,Vertical};
+    public motionDirection direction = motionDirection.Horizontal;
+    public float spinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+    public bool doSpin = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(direction){
+        case motionDirection.Spin:
+            //spin in up direction
+            gameObject.transform.Rotate(Vector3.up*spinSpeed*Time.deltaTime);
+            break;
+        
+        case motionDirection.Horizontal:
+            //motion in right direction
+            gameObject.transform.Translate(Vector3.right*Mathf.Cos(Time.timeSinceLevelLoad)*motionMagnitude);
+
+            if(doSpin){
+               gameObject.transform.Rotate(Vector3.up*spinSpeed*Time.deltaTime); 
+            }
+            break;
+ 
+        case motionDirection.Vertical:
+            //motion in up direction
+            gameObject.transform.Translate(Vector3.up*Mathf.Cos(Time.timeSinceLevelLoad)*motionMagnitude);
+            
+            if(doSpin){
+               gameObject.transform.Rotate(Vector3.up*spinSpeed*Time.deltaTime); 
+            }
+            break;
+        }
+    }
+    
+}
